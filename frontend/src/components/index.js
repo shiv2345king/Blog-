18228096222d287a4e8e580611e5d8b82e533ad5
Shivam Gupta <shivam.gupta19112003@gmail.com>
2026-05-21 18:13:37 +0530

    finally

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
-import Container from "./Container/Container.jsx";
+import Container from "./container/Container.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Logo from "./Logo.jsx";
 import Input from "./Input.jsx";
